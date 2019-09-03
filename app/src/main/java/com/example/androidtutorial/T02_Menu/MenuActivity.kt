f49dc8a2681d529928311096ae26eca7af149d7b
Your Name <you@example.com>
2019-09-03 17:20:28 +0900

    menu

--- app/src/main/java/com/example/androidtutorial/T02_Menu/MenuActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T02_Menu/MenuActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidtutorial.T02_Menu
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
@@ -16,5 +18,13 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_item1){
+            Toast.makeText(this, "menu item selcted",
+                Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
