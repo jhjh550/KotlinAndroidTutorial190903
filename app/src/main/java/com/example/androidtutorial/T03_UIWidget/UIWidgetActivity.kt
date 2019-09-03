f7301a68496c2f244151916644b750283b1b361e
Your Name <you@example.com>
2019-09-03 17:44:20 +0900

    button

---
+++ app/src/main/java/com/example/androidtutorial/T03_UIWidget/UIWidgetActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidtutorial.T03_UIWidget
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_uiwidget.*
+
+class UIWidgetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uiwidget)
+
+        btnHello.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
