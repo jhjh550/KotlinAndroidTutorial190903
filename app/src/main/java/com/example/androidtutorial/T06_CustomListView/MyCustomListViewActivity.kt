8a6ea9fdbcd034b8a15004f45e84a1fbc3072302
Your Name <you@example.com>
2019-09-04 11:43:18 +0900

    custom listview

--- app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidtutorial.T06_CustomListView
 
 import android.os.Bundle
+import android.widget.BaseAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
@@ -13,13 +14,23 @@ class MyCustomListViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_custom_list_view)
         generateData()
+
     }
     private fun generateData(){
+        val icons = arrayOf( R.drawable.ic_3d_rotation_black_24dp,
+            R.drawable.ic_ac_unit_black_24dp,
+            R.drawable.ic_access_alarm_black_24dp )
+
         for(i in 0..100){
+            val icon = icons[i%3]
             myList.add(
-                MyData("title $i", "desc $i",
-                R.drawable.ic_3d_rotation_black_24dp)
+                MyData("title $i", "desc $i", icon)
             )
         }
+    }
+
+
+    class MyCustomAdapter: BaseAdapter(){
+
     }
 }
