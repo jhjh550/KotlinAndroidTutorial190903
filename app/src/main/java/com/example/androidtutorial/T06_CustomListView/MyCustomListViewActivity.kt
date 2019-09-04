e14d13986b8a894fe0f66a4b70449dcf286edff4
Your Name <you@example.com>
2019-09-04 12:11:24 +0900

    custom listview

--- app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
@@ -1,7 +1,12 @@
 package com.example.androidtutorial.T06_CustomListView
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
@@ -30,7 +35,34 @@ class MyCustomListViewActivity : AppCompatActivity() {
     }
 
 
-    class MyCustomAdapter: BaseAdapter(){
+    inner class MyCustomAdapter: BaseAdapter(){
+        override fun getView(position: Int, converterView: View?, parent: ViewGroup?): View {
+            val v = LayoutInflater.from(parent?.context)
+                    .inflate(R.layout.item_custom_listview, parent, false)
+
+            val textViewTitle: TextView = v.findViewById(R.id.titleTextView)
+            val textViewDesc: TextView = v.findViewById(R.id.descTextView)
+            val itemImageView: ImageView = v.findViewById(R.id.itemImageView)
+
+            val data = myList[position]
+            textViewTitle.text = data.title
+            textViewDesc.text = data.desc
+            itemImageView.setImageResource(data.img)
+
+            return v
+        }
+
+        override fun getItem(position: Int): Any {
+            return myList.get(position)
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return myList.size
+        }
 
     }
 }
