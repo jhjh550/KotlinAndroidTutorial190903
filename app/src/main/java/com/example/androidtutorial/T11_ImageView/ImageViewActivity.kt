50cfc837120058a4887df887dfd48b851f0ae72b
Your Name <you@example.com>
2019-09-04 18:22:43 +0900

    imageview

--- app/src/main/java/com/example/androidtutorial/T11_ImageView/ImageViewActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T11_ImageView/ImageViewActivity.kt
@@ -2,7 +2,9 @@ package com.example.androidtutorial.T11_ImageView
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_image_view.*
 
 class ImageViewActivity : AppCompatActivity() {
 
@@ -10,6 +12,7 @@ class ImageViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_view)
 
-
+        val path = "https://api.androidhive.info/music/images/adele.png"
+        Glide.with(this).load(path).into(myImageView)
     }
 }
