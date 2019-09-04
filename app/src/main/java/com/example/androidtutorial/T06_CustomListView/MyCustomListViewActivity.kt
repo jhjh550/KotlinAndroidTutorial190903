96dc4e7c5e49f6f212f262315d8fcded88c59033
Your Name <you@example.com>
2019-09-04 11:23:08 +0900

    custom listview

--- app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
@@ -5,9 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
 class MyCustomListViewActivity : AppCompatActivity() {
+    data class MyData(val title:String, val desc:String, val img:Int)
+
+    val myList = ArrayList<MyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_custom_list_view)
+        generateData()
+    }
+    private fun generateData(){
+        for(i in 0..100){
+            myList.add(
+                MyData("title $i", "desc $i",
+                R.drawable.ic_3d_rotation_black_24dp)
+            )
+        }
     }
 }
