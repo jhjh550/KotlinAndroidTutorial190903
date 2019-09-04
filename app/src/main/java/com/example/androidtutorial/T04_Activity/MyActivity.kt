bc89709a55f74efc2e8ad4ca7f1c6fcde9cd9169
Your Name <you@example.com>
2019-09-04 10:21:09 +0900

    activity

--- app/src/main/java/com/example/androidtutorial/T04_Activity/MyActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T04_Activity/MyActivity.kt
@@ -1,12 +1,17 @@
 package com.example.androidtutorial.T04_Activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 import kotlinx.android.synthetic.main.activity_my_activity.*
 
 class MyActivity : AppCompatActivity() {
+
+    //private static final int REQ_CODE = 100
+    private val REQ_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +20,22 @@ class MyActivity : AppCompatActivity() {
         btnStart.setOnClickListener {
             val intent = Intent(this, NewActivity::class.java)
             intent.putExtra("hello", 123)
-            startActivity(intent)
+//            startActivity(intent)
+            startActivityForResult(intent, REQ_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                data?.let {
+                    val str = it.getStringExtra("resValue")
+                    Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+                }
+
+            }
         }
     }
 
