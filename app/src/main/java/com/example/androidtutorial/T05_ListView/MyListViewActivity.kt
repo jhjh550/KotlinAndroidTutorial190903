870f7179363660e2addca439d5eb36660b43058f
Your Name <you@example.com>
2019-09-04 11:14:55 +0900

    custom listview

--- app/src/main/java/com/example/androidtutorial/T05_ListView/MyListViewActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T05_ListView/MyListViewActivity.kt
@@ -1,8 +1,11 @@
 package com.example.androidtutorial.T05_ListView
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_my_list_view.*
 
 class MyListViewActivity : AppCompatActivity() {
 
@@ -10,8 +13,18 @@ class MyListViewActivity : AppCompatActivity() {
         "hello", "world", "android", "kotlin",
         "hello", "world", "android", "kotlin",
         "hello", "world", "android", "kotlin")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_list_view)
+
+        val adapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, myData)
+        myListView.adapter = adapter
+
+        myListView.setOnItemClickListener { adapterView, view, position, id ->
+            val str = myData[position]
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidtutorial/T06_CustomListView/MyCustomListViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidtutorial.T06_CustomListView
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.R
+
+class MyCustomListViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_custom_list_view)
+    }
+}
