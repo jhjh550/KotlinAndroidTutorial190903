44ed54e24d266857c36bdec8475850eaa08f4cc3
Your Name <you@example.com>
2019-09-04 15:43:46 +0900

    async task

--- app/src/main/java/com/example/androidtutorial/T09_AsyncTask/AsyncTaskActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T09_AsyncTask/AsyncTaskActivity.kt
@@ -4,22 +4,29 @@ import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_async_task.*
 
 class AsyncTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_task)
+        btnStart.setOnClickListener {
+            val task = MyTask()
+            task.execute(30)
+        }
     }
 
-    class MyTask: AsyncTask<Int, Float, String>(){
+    inner class MyTask: AsyncTask<Int, Float, String>(){
 
         override fun onProgressUpdate(vararg values: Float?) {
             super.onProgressUpdate(*values)
+            btnStart.setText("count ${values[0]}")
         }
 
-        override fun doInBackground(vararg p0: Int?): String {
-            for(i in 0..100){
+        override fun doInBackground(vararg params: Int?): String {
+            val start = params[0] ?: 0
+            for(i in start..100){
                 Thread.sleep(100)
                 publishProgress(i.toFloat())
             }
