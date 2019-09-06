e7da43565212ac0d3e334f1ca1bacc7d313025a6
Your Name <you@example.com>
2019-09-06 17:11:09 +0900

    style

---
+++ app/src/main/java/com/example/androidtutorial/T23_Style/MyStyleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidtutorial.T23_Style
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.R
+
+class MyStyleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_style)
+    }
+}
