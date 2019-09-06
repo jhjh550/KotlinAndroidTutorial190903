bf04a6bb10da41d5c986fe14c73c1f2fcd8e52ff
Your Name <you@example.com>
2019-09-06 14:51:05 +0900

    fragment

---
+++ app/src/main/java/com/example/androidtutorial/T21_fragment/MyFragmentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidtutorial.T21_fragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.R
+
+class MyFragmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+    }
+}
