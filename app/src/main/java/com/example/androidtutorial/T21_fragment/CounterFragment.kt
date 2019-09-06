49ef5662055fc5b55b6198fa6868845e6552884d
Your Name <you@example.com>
2019-09-06 15:49:26 +0900

    fragment

--- app/src/main/java/com/example/androidtutorial/T21_fragment/CounterFragment.kt
+++ app/src/main/java/com/example/androidtutorial/T21_fragment/CounterFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.androidtutorial.R
 import kotlinx.android.synthetic.main.fragment_counter.*
@@ -26,7 +25,8 @@ class CounterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnIncrease.setOnClickListener {
-            Toast.makeText(activity, "hello world", Toast.LENGTH_SHORT).show()
+            val value = countTextView.text.toString().toInt()
+            countTextView.text = "${value+1}"
         }
     }
 
--- app/src/main/java/com/example/androidtutorial/T21_fragment/MyFragmentActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T21_fragment/MyFragmentActivity.kt
@@ -1,13 +1,36 @@
 package com.example.androidtutorial.T21_fragment
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_fragment.*
 
-class MyFragmentActivity : AppCompatActivity() {
+class MyFragmentActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        btnAdd.setOnClickListener(this)
+        btnRemove.setOnClickListener(this)
+        btnReplace.setOnClickListener(this)
+        btnHide.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        val fragment = supportFragmentManager.findFragmentById(R.id.containerLayout)
+        when(view?.id){
+            R.id.btnAdd->{
+                if(fragment == null){
+                    val newFragment = CounterFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .add(R.id.containerLayout, newFragment, "counter")
+                        .commit()
+                }
+            }
+            R.id.btnRemove->{}
+        }
     }
 }
