21d78c88d21e8215ccd2f616757e6d05bf2b74f4
Your Name <you@example.com>
2019-09-06 16:42:21 +0900

    fragment

--- app/src/main/java/com/example/androidtutorial/T21_fragment/MyFragmentActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T21_fragment/MyFragmentActivity.kt
@@ -27,10 +27,49 @@ class MyFragmentActivity : AppCompatActivity(), View.OnClickListener {
                     supportFragmentManager
                         .beginTransaction()
                         .add(R.id.containerLayout, newFragment, "counter")
+                        .addToBackStack(null)
                         .commit()
                 }
             }
-            R.id.btnRemove->{}
+            R.id.btnRemove->{
+                if(fragment != null){
+                    supportFragmentManager
+                        .beginTransaction()
+                        .remove(fragment)
+                        .commit()
+                }
+            }
+            R.id.btnReplace->{
+                if(fragment != null){
+                    val newFragment = if(fragment.tag.equals("counter")){
+                        BlankFragment()
+                    }else{
+                        CounterFragment()
+                    }
+                    val tag = if(newFragment is CounterFragment) "counter" else "blank"
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.containerLayout, newFragment, tag)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            }
+            R.id.btnHide->{
+                if(fragment != null){
+                    if(fragment.isHidden) {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .show(fragment)
+                            .commit()
+                    }else{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .hide(fragment)
+                            .commit()
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/androidtutorial/T22_ViewPager/MyViewPagerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidtutorial.T22_ViewPager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.R
+
+class MyViewPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_view_pager)
+    }
+}
