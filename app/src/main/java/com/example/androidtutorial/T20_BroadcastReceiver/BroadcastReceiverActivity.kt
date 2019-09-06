30394ee38300eec7a51d2fc220a27dd46b2e70b5
Your Name <you@example.com>
2019-09-06 14:22:12 +0900

    broadcast receiver

--- app/src/main/java/com/example/androidtutorial/T20_BroadcastReceiver/BroadcastReceiverActivity.kt
+++ app/src/main/java/com/example/androidtutorial/T20_BroadcastReceiver/BroadcastReceiverActivity.kt
@@ -5,6 +5,7 @@ import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_broadcast_receiver.*
 
 class BroadcastReceiverActivity : AppCompatActivity() {
 
@@ -13,9 +14,17 @@ class BroadcastReceiverActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast_receiver)
 
+        btnSend.setOnClickListener {
+            val intent = Intent()
+            intent.action = "hello.world.android.kotlin"
+            sendBroadcast(intent)
+        }
+
         receiver = MyReceiver()
         val intentFilter = IntentFilter()
+        intentFilter.addAction("hello.world.android.kotlin")
         intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
         registerReceiver(receiver, intentFilter)
     }
 
--- app/src/main/java/com/example/androidtutorial/T20_BroadcastReceiver/MyReceiver.kt
+++ app/src/main/java/com/example/androidtutorial/T20_BroadcastReceiver/MyReceiver.kt
@@ -8,7 +8,18 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        Toast.makeText(context, "hello airplane  mode",
-            Toast.LENGTH_SHORT).show()
+        if(intent.action.equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+            Toast.makeText(
+                context, "hello airplane  mode",
+                Toast.LENGTH_SHORT
+            ).show()
+        }else if(intent.action.equals(Intent.ACTION_POWER_CONNECTED)){
+            Toast.makeText(
+                context, "power connected",
+                Toast.LENGTH_SHORT
+            ).show()
+        }else if (intent.action.equals("hello.world.android.kotlin")){
+
+        }
     }
 }
